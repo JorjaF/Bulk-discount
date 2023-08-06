60c0ad5665db16923af92d26c47c6d6fba57aac5
Jorja Fleming <aset284@gmail.com>
2023-08-06 16:15:14 -0600

    Refactored bulk_item

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -9,24 +9,12 @@ RSpec.describe "bulk discount index" do
     @bulk_discount_3 = BulkDiscount.create!(name: "Spring Fling", percentage: 0.30, quantity: 100, merchant_id: @merchant2.id)
   end
 
-  it "can add a new bulk discount" do
-    
+  it "has a link to a new bulk discount page" do
     visit merchant_bulk_discounts_path(@merchant1)
-        
+          
     click_link 'Create New Discount'
-    
+      
     expect(page).to have_content('New Bulk Discount')
-
-    fill_in 'Name', with: 'New Discount'
-    fill_in 'percentage', with: '.15'
-    fill_in 'Quantity', with: '25'
-    click_button 'Submit'
-
-    expect(page).to have_content('Bulk Discounts Index')
-
-    expect(page).to have_content('New Discount')
-    expect(page).to have_content('Percentage: 15%')
-    expect(page).to have_content('Quantity: 25')
   end
 
   it "can delete a bulk discount" do
---
+++ spec/features/bulk_discounts/new_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe "bulk discount new" do
+  before :each do
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @merchant2 = Merchant.create!(name: "Jewelry Jar")
+    @bulk_discount_1 = BulkDiscount.create!(name: "Fire Sale", percentage: 0.1, quantity: 10, merchant_id: @merchant1.id)
+    @bulk_discount_2 = BulkDiscount.create!(name: "Going Out of Business", percentage: 0.25, quantity: 20, merchant_id: @merchant1.id)
+    @bulk_discount_3 = BulkDiscount.create!(name: "Spring Fling", percentage: 0.30, quantity: 100, merchant_id: @merchant2.id)
+  end
+
+  it "can add a new bulk discount" do
+    visit new_merchant_bulk_discount_path(@merchant1)
+    
+    expect(page).to have_content('New Bulk Discount')
+
+    fill_in 'Name', with: 'New Discount'
+    fill_in 'percentage', with: '.15'
+    fill_in 'Quantity', with: '25'
+    click_button 'Submit'
+
+    expect(page).to have_content('Bulk Discounts Index')
+    
+    within("#bulk-discount-item-#{BulkDiscount.last.id}") do
+      expect(page).to have_content('New Discount')
+      expect(page).to have_content('Percentage: 15%')
+      expect(page).to have_content('Quantity: 25')
+    end
+  end
+  
+  it "can not build a new discount without all the fields filled in" do
+    
+    visit merchant_bulk_discounts_path(@merchant1)
+        
+    click_link 'Create New Discount'
+    
+    expect(page).to have_content('New Bulk Discount')
+
+    fill_in 'Name', with: 'New Discount'
+    fill_in 'percentage', with: '.15'
+    click_button 'Submit'
+
+    expect(page).to have_content("All fields must be completed, get your act together.")
+  end
+end
